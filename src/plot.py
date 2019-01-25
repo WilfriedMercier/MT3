d12af26752dea3836138a148e05a02d066a41ba5
Etudiant massonneau <massonne@pccloned.ias.u-psud.fr>
2019-01-25 14:49:14 +0100

    V.finale

--- src/plot.py
+++ src/plot.py
@@ -2,14 +2,10 @@ import matplotlib.pyplot as plt
 import healpy as hp
 
 
-freq  = [100] #,143,217,353,545,857]
+freq  = [100,143,217,353,545,857]
 for i in freq:
-<<<<<<< HEAD
-	maps = hp.read_map("/home/massonne/p1_massonne_mercier/data/HFI_256_"+str(i)+".fits")
-	hp.mollview(maps, title="galactique", coord=['G'], unit=r"$K_{CMB}$", norm="hist")#, min=-1, max=1, xsize=200)
-=======
-	map    = hp.read_map("data/HFI_256_"+str(i)+".fits")
-	hp.mollview(map, title="galactique", coord=['G'], unit="mK", norm="hist", xsize=400)
-	hp.mollview(map, title="galactique", coord=['G', 'E'], unit="mK", norm="hist", xsize=400)
->>>>>>> aab4853e1915d111ed447efaff808a691f0a0b61
+	maps    = hp.read_map("data/HFI_256_"+str(i)+".fits")
+	hp.mollview(maps, title="galactique", coord=['G'], unit="K", norm="hist", xsize=400)
+	hp.mollview(maps, title="ecliptique", coord=['G', 'E'], unit="K", norm="hist", xsize=400)
+
 plt.show()
