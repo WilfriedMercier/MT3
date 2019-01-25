bf9c2b02821c13b00d56ab324ed3f58dd6b5f81b
Etudiant massonneau <massonne@pccloned.ias.u-psud.fr>
2019-01-25 14:42:39 +0100

    a maj

--- src/plot.py
+++ src/plot.py
@@ -4,6 +4,6 @@ import healpy as hp
 
 freq  = [100,143,217,353,545,857]
 for i in freq:
-	map    = hp.read_map("data/HFI_256_"+str(i)+".fits")
-	hp.mollview(map, title="galactique", coord=['G'], unit="mK", norm="hist", min=-1, max=1, xsize=200)
+	maps = hp.read_map("/home/massonne/p1_massonne_mercier/data/HFI_256_"+str(i)+".fits")
+	hp.mollview(maps, title="galactique", coord=['G'], unit=r"$K_{CMB}$", norm="hist")#, min=-1, max=1, xsize=200)
 plt.show()
