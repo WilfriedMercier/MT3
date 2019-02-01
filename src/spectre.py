9e0d41b9c7f044d5cb75c104d06cf021845b53a0
Etudiant massonneau <massonne@edu-calcul1.ias.u-psud.fr>
2019-02-01 18:06:54 +0100

    coucou

--- src/spectre.py
+++ src/spectre.py
@@ -10,7 +10,9 @@ from healpy.sphtfunc import anafast
 
 ##Recup_carte
 
-cmb_maps=[]
+cmb_maps=['2048_convol']
+map_masque = hp.read_map("data/HFI_2048_convol_mask_25_percent.fits")
+ind_masque=np.array(np.where(map_masque==hp.UNSEEN)[0])
 
 for i in range(len(cmb_maps)):
 
@@ -22,12 +24,19 @@ for i in range(len(cmb_maps)):
 		array_maps[0,:]=maps
 	else:
 		array_maps[i,:]=maps
+	
+	array_masque=np.copy(array_maps)
+	array_masque[i,:][ind_masque]=hp.UNSEEN
+	
+	##hp.mollview(array_masque[i,:], title="Cartes avec masque", coord=['G'], unit=r"$K_{CMB}$", norm="hist", min=min(array_masque[i,:]), max=max(array_masque[i,:]))
+	##plt.show()
+	##exit()
 
 ##Power spectrum
-	CL=anafast(array_maps[i,:])
+	CL=anafast(array_masque[i,:])#,gal_cut=17)
 	l=np.arange(len(CL))
 	plt.plot(l,CL*(l*(l+1))/(2*np.pi),'*',label=cmb_maps[i])
-	plt.xscale('log')
+#	plt.xscale('log')
 	plt.legend(loc='best')
 
 plt.show()
