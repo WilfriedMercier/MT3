dfb057c84e0086478398f2420b311562c24bd9f5
wilfried <wilfried.mercier58@gmail.com>
2019-02-08 18:47:19 +0100

    no idea 2

--- src/spectre.py
+++ src/spectre.py
@@ -6,11 +6,11 @@ import numpy as np
 import healpy.pixelfunc as px
 from sys import exit
 from numpy.linalg import inv
-from healpy.sphtfunc import anafast
+from healpy.sphtfunc import anafast, pixwin, gauss_beam
 
 ##Recup_carte
 
-cmb_maps=['256Norm1P']
+cmb_maps=['2048_convol']
 map_masque = hp.read_map("data/HFI_2048_convol_mask_25_percent.fits")
 ind_masque=np.array(np.where(map_masque==hp.UNSEEN)[0])
 
@@ -33,7 +33,10 @@ for i in range(len(cmb_maps)):
 	##exit()
 
 ##Power spectrum
-	CL=anafast(array_masque[i,:])#,gal_cut=17)
+	CL=anafast(array_masque[i,:], lmax=8192)#,gal_cut=17)
+
+	CL /= pixwin(2048)**2 * (1-len(ind_masque)/len(array_maps[i,:])) * gauss_beam(4.33*np.pi/(180*60), lmax=8192)
+
 	l=np.arange(len(CL))
 	plt.plot(l,CL*(l*(l+1))/(2*np.pi),'*',label=cmb_maps[i])
 #	plt.xscale('log')
